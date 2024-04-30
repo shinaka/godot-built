d811bf916c24ed413f4e0c85ea973e7bc7cc0dfe
JimW <jimweinhart@gmail.com>
2024-04-30 02:11:25 -0400

    yabb

--- build_osx.sh
+++ build_osx.sh
@@ -1,6 +1,9 @@
+cd godot-built
+
 scons p=macos arch=arm64 target=editor module_mono_enabled=yes
 scons p=macos arch=arm64 target=template_debug module_mono_enable=true
 scons p=macos arch=arm64 target=template_release module_mono_enabled=true
 
 bin/godot.macos.editor.arm64.mono --headless --generate-mono-glue modules/mono/glue
 ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=macos --push-nupkgs-local ~/Nuget
+cd ..
